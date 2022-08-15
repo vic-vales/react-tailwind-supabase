919db52a27811b7084b837cff7b11cf773f05b12
Vic Tirta <vtirta@gmail.com>
2022-08-15 06:40:36 -0700

    Added Tailwind Hello World

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# React + Tailwind
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app) and [Tailwindcss](https://tailwindcss.com/docs/guides/create-react-app)
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
